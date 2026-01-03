0229a6773ea1eaf5c6a9019cc282391719c1333a
USER <USER@DESKTOP-KU063NL>
2026-01-03 11:39:48 +0530

    new file

---
+++ SampleJavaProjects/src/Test021Practice/Test001.java
@@ -0,0 +1,12 @@
+package Test021Practice;
+
+public class Test001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I ama amar");
+
+	}
+
+}
