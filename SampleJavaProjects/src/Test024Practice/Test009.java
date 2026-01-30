93ab844b457cf204a456ee10b1c692fa3f245e37
USER <USER@DESKTOP-KU063NL>
2026-01-30 19:49:03 +0530

    fff

--- SampleJavaProjects/src/Test024Practice/Test009.java
+++ SampleJavaProjects/src/Test024Practice/Test009.java
@@ -5,12 +5,8 @@ public class Test009 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-<<<<<<< HEAD
-		//List1
-=======
-		//amar
->>>>>>> branch 'amar' of https://github.com/amarpadavath/SampleJavaProjects
-		//Set
+		
+		//Set2
 		//Queue
 		//Map
 		//Collection
