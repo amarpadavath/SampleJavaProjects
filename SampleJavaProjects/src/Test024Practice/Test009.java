a2d5725c62d99c273b7329e73ada3438763f4a5c
USER <USER@DESKTOP-KU063NL>
2026-01-30 20:55:17 +0530

    resolved confict

--- SampleJavaProjects/src/Test024Practice/Test009.java
+++ SampleJavaProjects/src/Test024Practice/Test009.java
@@ -5,17 +5,17 @@ public class Test009 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-<<<<<<< HEAD
+//<<<<<<< HEAD
 		
 		//Set2
-=======
-<<<<<<< HEAD
+//=======
+//<<<<<<< HEAD
 		//List1
-=======
+//=======
 		//amar
->>>>>>> branch 'amar' of https://github.com/amarpadavath/SampleJavaProjects
+//>>>>>>> branch 'amar' of https://github.com/amarpadavath/SampleJavaProjects
 		//Set1
->>>>>>> branch 'amar' of https://github.com/amarpadavath/SampleJavaProjects
+//>>>>>>> branch 'amar' of https://github.com/amarpadavath/SampleJavaProjects
 		//Queue
 		//Map
 		//Collection
